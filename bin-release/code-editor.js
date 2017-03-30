7c10e9f6bb64535a6fc6538f81bbb73c7aafa3be
dungang <dungang@126.com>
2017-03-30 15:38:28 +0800

    添加发布包，方便直接使用不用编译

---
+++ bin-release/code-editor.js
@@ -0,0 +1,131 @@
+/**
+ * Created by Lenovo on 2017/3/28.
+ * @author  dungang
+ */
+
+var CodeEditor = (function(){
+    function CodeEditor(){
+        this.swfContainerId = '';
+        this.swfTextAreaId='';
+        this.swfEditorObject='CodeHighlightEditor.swf';
+        this.swfVersion='10.0.0';
+        this.height='420';
+        this.width='100%';
+        this.previewButtonId = 'preview';
+
+        // indicate the parser, aspx / csharp / javascript / css / vbscript / html / xml / php / phpcode
+        this.parser='php';
+
+        // set the editor to read-only mode
+        this.readOnly=false;
+
+        // the editor detects client installed fonts and use preferred fonts if installed.
+        // NOTE: the charactor '|' is required at the begin and end of the list
+        this.fonts='|Consolas|Courier New|Courier|Arial|Tahoma|';
+        this.fontSize = 12;
+
+        this.add.apply(this,arguments);
+
+        this.flashId = 'ctrFlash' + this.swfTextAreaId.replace('-','');
+
+        this._editorText = document.getElementById(this.swfTextAreaId);
+        if(this._editorText) {
+            this._editorText.style.display = 'none';
+        }
+    }
+
+    CodeEditor.prototype.add = function(options){
+        for(var prop in options) {
+            if(this.hasOwnProperty(prop)) {
+                this[prop] = options[prop];
+            }
+        }
+    };
+
+    CodeEditor.prototype.preview = function () {
+        var left = (window.outerWidth -600)/2;
+        var preview = open("", "code-editor-preview",
+            "top=100,left="+left+",height=400,width=600,location=no,status=no,menubar=yes,toolbar=no");
+        preview.document.open();
+        preview.document.write(this._editorText.value);
+        preview.document.close();
+    };
+
+    CodeEditor.prototype.getEditorObject = function () {
+        return document.getElementById(this.flashId);
+    };
+
+    CodeEditor.prototype.render = function () {
+
+        var flashVars = {
+            flashId:this.flashId,
+            textareaId:this.swfTextAreaId,
+            parser: this.parser,
+            readOnly: this.readOnly,
+            preferredFonts : this.fonts,
+            fontSize : this.fontSize,
+            onload: "CodeEditor.load",
+            onchange: "CodeEditor.change"
+        };
+        var params = { menu: "false", /* wmode : "transparent", */allowscriptaccess : "always" };
+        swfobject.embedSWF(this.swfEditorObject+'?now=' + (new Date()).getTime(),
+            this.swfContainerId, this.width, this.height, this.swfVersion, null,
+            flashVars, params, {
+                id:this.flashId,
+                name:this.flashId
+            }
+        );
+
+        var _this = this;
+        var preview = document.getElementById(this.previewButtonId);
+        if (preview) {
+            preview.onclick=function (event) {
+                event.preventDefault();
+                _this.preview();
+            };
+        }
+        return this;
+    };
+
+    CodeEditor.prototype.changeParser = function (langauge) {
+        this.getEditorObject().setParser(langauge);
+    }
+
+    CodeEditor.prototype.setContent = function (code) {
+        this.getEditorObject().setText(code);
+        this._editorText.value = code;
+    };
+
+    CodeEditor.prototype.reload = function () {
+        this.getEditorObject().setText(this._editorText.value);
+    };
+
+    CodeEditor.create = function (options) {
+        return new CodeEditor(options).render();
+    };
+
+    CodeEditor.load = function (swfId,textareaId) {
+        var editorObj = document.getElementById(swfId);
+        var textarea = document.getElementById(textareaId);
+        editorObj.setText(textarea.value);
+        CodeEditor.onAfterLoaded.call(editorObj,textarea);
+    };
+
+    CodeEditor.change = function (textareaId,code) {
+        var textarea = document.getElementById(textareaId);
+        textarea.value = code;
+        CodeEditor.onAfterChanged.call(textarea,code);
+    };
+
+    //callback
+    CodeEditor.onAfterLoaded = function (editorObj,textarea) {
+
+    };
+
+    //callback
+    CodeEditor.onAfterChanged = function (textarea,code) {
+
+    };
+
+    return CodeEditor;
+}());
